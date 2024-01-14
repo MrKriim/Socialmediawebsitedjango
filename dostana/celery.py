bec0ac3ca20c1f7e2355e8bf0c124465b51721d6
AbdulKareemKhan <101586794+MrKriim@users.noreply.github.com>
2024-01-14 13:09:58 +0500

    Update celery.py

--- dostana/celery.py
+++ dostana/celery.py
@@ -1,7 +1,7 @@
 # dostana/celery.py
 from celery import Celery
 
-app = Celery('dostana', broker='pyamqp://guest:guest@localhost//')  # Update the broker URL accordingly
+app = Celery('dostana', broker='redis://localhost:6379/0')  # Update the broker URL accordingly
 
 # Load task modules from all registered Django app configs.
 app.config_from_object('django.conf:settings', namespace='CELERY')
